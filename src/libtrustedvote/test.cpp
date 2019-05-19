f017687dfb7bf94084de50d943610016e566e040
Putta Khunchalee <ultimaweapon@outlook.com>
2019-05-19 18:46:38 +0700

    Initialize unit tests

---
+++ src/libtrustedvote/test.cpp
@@ -0,0 +1,13 @@
+#define BOOST_TEST_MODULE Trusted Vote Core Tests
+
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(dummy_test)
+{
+	// TODO: remove this when there is other tests
+}
+
+int main(int argc, char *argv[])
+{
+	return boost::unit_test::unit_test_main(init_unit_test, argc, argv);
+}
