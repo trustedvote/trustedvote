06dca77dfa7f34109f999cd6cadd28ce4f4b7620
Putta Khunchalee <ultimaweapon@outlook.com>
2019-06-09 17:37:12 +0700

    Start servers

--- src/trustedvoted/main.cpp
+++ src/trustedvoted/main.cpp
@@ -1,25 +1,40 @@
 #include <trustedvote/config.hpp>
 
+#include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/signal_set.hpp>
+#include <boost/system/error_code.hpp>
 
 #include <csignal>
 #include <cstdlib>
 #include <exception>
 #include <iostream>
 #include <ostream>
+#include <vector>
 
 static trustedvote::config conf;
 static boost::asio::io_context asio;
+static std::vector<boost::asio::ip::tcp::acceptor> servers;
 
 static void terminate()
 {
-	std::cout << "Terminating..." << std::endl;
+	for (auto &server : servers) {
+		std::cout << "Stopping server " << server.local_endpoint() << ".";
+		std::cout << std::endl;
+
+		server.close();
+	}
 }
 
 static void on_terminate_signal(const boost::system::error_code& e, int s)
 {
 	terminate();
+}
+
+static void accept_connection(
+	boost::system::error_code const &ec,
+	boost::asio::ip::tcp::socket s)
+{
 }
 
 int main(int argc, char *argv[])
@@ -37,6 +52,18 @@ int main(int argc, char *argv[])
 		std::cout << trustedvote::config::default_json_file << ": " << e.what();
 		std::cout << std::endl;
 		return EXIT_FAILURE;
+	}
+
+	// start servers.
+	for (auto &iface : conf.network.server.interfaces) {
+		boost::asio::ip::tcp::endpoint addr;
+
+		addr.address(boost::asio::ip::make_address(iface));
+		addr.port(conf.network.server.port);
+
+		std::cout << "Starting server " << addr << "." << std::endl;
+
+		servers.emplace_back(asio, addr).async_accept(accept_connection);
 	}
 
 	// setup signal handling
