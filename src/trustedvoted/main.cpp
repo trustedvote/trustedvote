269733e039f6466308b045fb2f1571d90be959f2
Putta Khunchalee <ultimaweapon@outlook.com>
2019-06-16 14:22:45 +0700

    Add arguments parser for trustedvoted

--- src/trustedvoted/main.cpp
+++ src/trustedvoted/main.cpp
@@ -1,8 +1,11 @@
+#include "options.hpp"
+
 #include <trustedvote/config.hpp>
 
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/signal_set.hpp>
+#include <boost/program_options.hpp>
 #include <boost/system/error_code.hpp>
 
 #include <csignal>
@@ -12,6 +15,7 @@
 #include <ostream>
 #include <vector>
 
+static options opts;
 static trustedvote::config conf;
 static boost::asio::io_context asio;
 static std::vector<boost::asio::ip::tcp::acceptor> servers;
@@ -31,6 +35,35 @@ static void on_terminate_signal(const boost::system::error_code& e, int s)
 	terminate();
 }
 
+static bool parse_arguments(int argc, char *argv[])
+{
+	// setup available options
+	boost::program_options::options_description desc("Available options");
+
+	desc.add_options()
+		("help,h", "show this help")
+	;
+
+	// parse
+	boost::program_options::variables_map parsed;
+
+	boost::program_options::store(
+		boost::program_options::parse_command_line(argc, argv, desc),
+		parsed
+	);
+
+	boost::program_options::notify(parsed);
+
+	if (parsed.count("help")) {
+		std::cout << "Usage: " << argv[0] << " [OPTION]..." << std::endl;
+		std::cout << std::endl;
+		std::cout << desc;
+		return false;
+	}
+
+	return true;
+}
+
 static void accept_connection(
 	boost::system::error_code const &ec,
 	boost::asio::ip::tcp::socket s)
@@ -39,6 +72,16 @@ static void accept_connection(
 
 int main(int argc, char *argv[])
 {
+	// parse arguments
+	try {
+		if (!parse_arguments(argc, argv)) {
+			return EXIT_SUCCESS;
+		}
+	} catch (std::exception &e) {
+		std::cerr << e.what() << "." << std::endl;
+		return EXIT_FAILURE;
+	}
+
 	// load config
 	std::cout << "Loading " << trustedvote::config::default_json_file << ".";
 	std::cout << std::endl;
@@ -50,7 +93,7 @@ int main(int argc, char *argv[])
 	} catch (std::exception &e) {
 		std::cout << "Failed to load ";
 		std::cout << trustedvote::config::default_json_file << ": " << e.what();
-		std::cout << std::endl;
+		std::cout << "." << std::endl;
 		return EXIT_FAILURE;
 	}
 
@@ -75,12 +118,12 @@ int main(int argc, char *argv[])
 	sigs.async_wait(on_terminate_signal);
 
 	// enter event loop
-	std::cout << "Ready" << std::endl;
+	std::cout << "Ready." << std::endl;
 
 	try {
 		asio.run();
 	} catch (std::exception& e) {
-		std::cerr << e.what() << std::endl;
+		std::cerr << e.what() << "." << std::endl;
 		return EXIT_FAILURE;
 	}
 
